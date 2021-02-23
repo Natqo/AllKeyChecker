a9f3d3d336e8b52b970368b674a8afe28b16c0bc
Natqo <natqowskyy@gmail.com>
2021-02-23 14:11:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,33 @@ import bs4
 import requests
 import os
 import sys
+import json
 import rich
+import msvcrt
 from rich.console import Console
 from rich.table import Table
 
 
 def getGameInfo(x):
     a1 = x.a(class_="search-results-row-price")[0].text
-    return {"name": x.a(class_="search-results-row-game-title")[0].text, "price": " ".join(a1.split()), "link": x.a['href']}
+    return {"name": x.a(class_="search-results-row-game-title")[0].text, "price": ' '.join(a1.split()), "link": x.a['href']}
+
+
+def steamPrice(x):
+    r = requests.get(
+        "https://api.steampowered.com/ISteamApps/GetAppList/v0002/")
+    jsondata = json.loads(r.content)
+    final = ""
+    for x in jsondata['applist']['apps']:
+        if x['name'].lower() == ft:
+            final = x['appid']
+            break
+    if final == "":
+        return "not listed"
+    r = requests.get(
+        f"https://store.steampowered.com/api/appdetails?appids={final}&cc=de")
+    jsondata = json.loads(r.content)
+    return jsondata[f'{final}']['data']['price_overview']['final_formatted']
 
 
 console = Console()
@@ -22,12 +41,12 @@ table.add_column(header="Link", no_wrap=True,
                  header_style="Bold Blue", style="Blue")
 
 console.clear()
-ft = input("game title: ").replace(" ", "+")
+ft = input("game title: ").lower()
+fft = ft.replace(" ", "+")
 
-txt = requests.get(
-    f"https://www.allkeyshop.com/blog/catalogue/search-{ft}/")
+txt = requests.get(f"https://www.allkeyshop.com/blog/catalogue/search-{fft}/")
 
-soup = bs4.BeautifulSoup(txt.text, 'html.parser')
+soup = bs4.BeautifulSoup(txt.content, 'html.parser')
 found = soup.find_all("li", class_="search-results-row")
 
 if len(found) == 0:
@@ -46,6 +65,8 @@ for x in range(0, max_entries):
     table.add_row(str(x+1), info['name'], info['price'], info['link'])
     table.add_row()
 
+secondmessage = f"[bold red]current steam price: [/bold red][yellow]{steamPrice(ft)}[/yellow]"
 print()
 console.print(table)
-console.print("created by:\nhttps://github.com/Natqo")
+console.print(secondmessage)
+console.print("\ncreated by:\nhttps://github.com/Natqo")
